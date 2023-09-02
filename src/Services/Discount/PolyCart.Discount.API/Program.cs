775ec5ee87bf17ba1c0048f65a4e549b5d06ec20
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-09-02 21:08:23 +0530

    Discount API implemented with PostgreSQL

--- src/Services/Discount/PolyCart.Discount.API/Program.cs
+++ src/Services/Discount/PolyCart.Discount.API/Program.cs
@@ -1,6 +1,11 @@
+using PolyCart.Discount.API.Extensions;
+using PolyCart.Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +13,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.Services.CreateScope().ServiceProvider.GetRequiredService<IHost>().MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ src/Services/Discount/PolyCart.Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using PolyCart.Discount.API.Entities;
+
+namespace PolyCart.Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
