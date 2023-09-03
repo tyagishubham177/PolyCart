b0e0f41b7c819f61cbf7e94e74573cab50ea5833
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-09-03 10:38:43 +0530

    Gprc code implemented

---
+++ src/Services/Discount/PolyCart.Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
+using PolyCart.Discount.Grpc.Entities;
+
+namespace PolyCart.Discount.Grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
