6fc513d70964d9d71b15765bb2b92ac83c0a98ae
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-09-08 16:58:44 +0530

    web app code copied

---
+++ src/WebApps/PolyCart.Web/Repositories/Interfaces/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using PolyCart.Web.Entities;
+using System.Threading.Tasks;
+
+namespace PolyCart.Web.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<Cart> GetCartByUserName(string userName);
+        Task AddItem(string userName, int productId, int quantity = 1, string color = "Black");
+        Task RemoveItem(int cartId, int cartItemId);
+        Task ClearCart(string userName);
+    }
+}
