1b3390e4eeed88ce387bb5de8117c226f592e81e
shubham tyagi <shubham.tyagi@soprasteria.com>
2023-09-08 18:05:42 +0530

    docker web app implemented

--- src/WebApps/PolyCart.Web/Pages/Cart.cshtml.cs
+++ src/WebApps/PolyCart.Web/Pages/Cart.cshtml.cs
@@ -4,30 +4,40 @@ using PolyCart.Web.Entities;
 using PolyCart.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PolyCart.Web.Services;
+using PolyCart.Web.Models;
 
 namespace PolyCart.Web
 {
     public class CartModel : PageModel
     {
-        private readonly ICartRepository _cartRepository;
+        private readonly IBasketService _basketService;
 
-        public CartModel(ICartRepository cartRepository)
+        public CartModel(IBasketService basketService)
         {
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
         }
 
-        public Entities.Cart Cart { get; set; } = new Entities.Cart();        
+        public BasketModel Cart { get; set; } = new BasketModel();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Cart = await _cartRepository.GetCartByUserName("test");            
+            var userName = "shub";
+            Cart = await _basketService.GetBasket(userName);
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostRemoveToCartAsync(int cartId, int cartItemId)
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
         {
-            await _cartRepository.RemoveItem(cartId, cartItemId);
+            var userName = "shub";
+            var basket = await _basketService.GetBasket(userName);
+
+            var item = basket.Items.Single(x => x.ProductId == productId);
+            basket.Items.Remove(item);
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+
             return RedirectToPage();
         }
     }
--- src/WebApps/PolyCart.Web/Pages/Order.cshtml.cs
+++ src/WebApps/PolyCart.Web/Pages/Order.cshtml.cs
@@ -4,25 +4,27 @@ using System.Threading.Tasks;
 using PolyCart.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PolyCart.Web.Models;
+using PolyCart.Web.Services;
 
 namespace PolyCart.Web
 {
     public class OrderModel : PageModel
     {
-        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderService _orderService;
 
-        public OrderModel(IOrderRepository orderRepository)
+        public OrderModel(IOrderService orderService)
         {
-            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
         }
 
-        public IEnumerable<Entities.Order> Orders { get; set; } = new List<Entities.Order>();
+        public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Orders = await _orderRepository.GetOrdersByUserName("test");
+            Orders = await _orderService.GetOrdersByUserName("shub");
 
             return Page();
-        }       
+        }
     }
 }
